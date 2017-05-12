ed6a2a5fc01d9d91bfdafa3ef972ac0ce2e246f4
jalips <link_521@hotmail.fr>
2017-05-12 15:10:00 +0200

    Add Get call with volley

---
+++ app/src/main/java/com/autoyas/app/autoyas/activities/MainActivity.java
@@ -0,0 +1,66 @@
+package com.autoyas.app.autoyas.activities;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.autoyas.app.autoyas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //makeDialog();
+
+        callNetwork();
+    }
+
+    private void makeDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hello W");
+        builder.setMessage("My Body");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Display some toast
+                Toast toast = Toast.makeText(com.autoyas.app.autoyas.activities.MainActivity.this, "Click positiv.", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void callNetwork() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, "http://www.google.fr/", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, "Response from API", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Error from api", Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(request);
+    }
+
+
+}
---
+++ app/src/main/java/com/autoyas/app/autoyas/entities/Device.java
@@ -0,0 +1,24 @@
+package com.autoyas.app.autoyas.entities;
+
+/**
+ * Created by link on 12/05/17.
+ */
+
+public class Device {
+
+    private String name;
+
+    public Device(){}
+
+    public Device(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
