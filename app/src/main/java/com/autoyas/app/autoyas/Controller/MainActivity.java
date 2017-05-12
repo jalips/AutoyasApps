17d270e88ea3424281cf48c25d5037b966df2706
jalips <link_521@hotmail.fr>
2017-05-12 14:43:26 +0200

    Continue but bug with Adapter

--- app/src/main/java/com/autoyas/app/autoyas/Controller/MainActivity.java
+++ app/src/main/java/com/autoyas/app/autoyas/Controller/MainActivity.java
@@ -1,7 +1,11 @@
 package com.autoyas.app.autoyas.Controller;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 import com.autoyas.app.autoyas.R;
 
@@ -11,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //makeDialog();
     }
+
+    private void makeDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hello W");
+        builder.setMessage("My Body");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Display some toast
+                Toast toast = Toast.makeText(MainActivity.this, "Click positiv.", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+
 }
