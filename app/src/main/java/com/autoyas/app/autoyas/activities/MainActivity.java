f4b10b0e65a2892032af85bf21881a67013ed75a
Manon Carbonnel <manon.carbonnel@gmail.com>
2017-05-12 16:38:49 +0200

    oups import

--- app/src/main/java/com/autoyas/app/autoyas/activities/MainActivity.java
+++ app/src/main/java/com/autoyas/app/autoyas/activities/MainActivity.java
@@ -19,7 +19,6 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.autoyas.app.autoyas.R;
-import com.github.mikephil.charting.charts.LineChart;
 
 public class MainActivity extends AppCompatActivity {
 
